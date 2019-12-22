f245d526bc55d98d1b086ebbb18fac0547470676
Daniel Lucazeau <daniel.lucazeau@antemeta.fr>
2019-12-22 22:58:58 +0100

    More unit tests

--- tslint-rules/noTruthyFalsyRule.spec.ts
+++ tslint-rules/noTruthyFalsyRule.spec.ts
@@ -8,9 +8,36 @@ describe('noTruthyFalsyRule test', () =>
     it(`use of toBeTruthy`, () =>
     {
         const src = `expect(xyz).toBeTruthy();`;
+        const failure = helper({ src, rule }).failures[0];
+
+        expect(failure.getFailure()).toBe(Rule.FAILURE_STRING);
+    });
+
+    it(`use of toBeTruthy`, () =>
+    {
+        const src = `expect(xyz).toBeTruthy();`;
         const result = helper({ src, rule });
 
         expect(result.errorCount).toBe(1);
+    });
+
+    it(`use of toBeTruthy`, () =>
+    {
+        const src = `expect(xyz).toBeTruthy();`;
+        const failure = helper({ src, rule }).failures[0];
+
+        expect(failure.getFailure()).toBe(Rule.FAILURE_STRING);
+    });
+
+    it(`use of toBeTruthy`, () =>
+    {
+        const src = `expect(xyz).toBeTruthy();`;
+        const startPosition = src.indexOf('toBeTruthy');
+        const endPosition = startPosition + 'toBeTruthy'.length;
+        const failure = helper({ src, rule }).failures[0];
+
+        expect(failure.getStartPosition().getPosition()).toEqual(startPosition);
+        expect(failure.getEndPosition().getPosition()).toEqual(endPosition);
     });
 
     it(`use of toBeFalsy`, () =>
